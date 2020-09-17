f000922dd5583669c9488d1770c5a8530ff92428
brest computer club <brest-computer-club@protonmail.com>
2020-09-17 08:30:41 +0000

    add nix config

---
+++ dev.nix
@@ -0,0 +1,13 @@
+{pkgs ? import <nixpkgs> {}}:
+
+let 
+  src = builtins.fetchGit {
+    url = "https://gitlab.com/brestcc/nix";
+    ref = "master"; 
+  };
+  altPkgs = import src pkgs; 
+in 
+pkgs.mkShell {
+  buildInputs = with pkgs; 
+    altPkgs.bundles.elm pkgs;
+}
